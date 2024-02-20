83c2744f048595cc17dfec1d600e2bec34ba206d
JamesGower0 <126535903+JamesGower0@users.noreply.github.com>
2024-02-20 18:11:45 +0000

    register, homepage, navbar structure fixed

---
+++ ECM-2434-main (fixed structure)/groupproject/application/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from . import forms
+
+# Create your views here.
+
+def qr(request):
+    return HttpResponse("placeholder for qr functionality")
+
+def navBar(request):
+    return render(request, 'navBar.html')
+
+def home(request):
+    template = "home.html"
+    context = {}
+    return render(request, "home.html", context)
+
+def register(response):
+    if response.method == "POST":
+        form = forms.RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/application/qr/")
+            #need to redirect to login page or home page?
+    else:
+        form = forms.RegisterForm()
+
+    return render(response, "register.html", {"form":form})
