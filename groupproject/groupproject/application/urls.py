c065df528135cc06c65350f5fa3cc1e2dee52711
Ashley Card <114603653+ashleycard@users.noreply.github.com>
2024-02-22 17:17:45 +0000

    Delete groupproject/groupproject/application directory

--- groupproject/groupproject/application/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("qr/", views.qr, name="qr"),
-    path("home/", views.home, name="home"),
-    path("navBar/", views.navBar, name="navBar"),
-    path("register/", views.register, name="register"),
-]
