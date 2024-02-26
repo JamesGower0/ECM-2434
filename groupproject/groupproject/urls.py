90dc789684eec895e570d4fc0a7df8b5208e881d
mafarl <marie.v.frolova@gmail.com>
2024-02-26 17:06:36 +0000

    leaderboard

--- groupproject/groupproject/urls.py
+++ groupproject/groupproject/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path("profile/", views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
     path('logout/', views.logout_view, name = 'logout'),
+    path('leaderboard/', views.leaderboard, name = 'leaderboard'),
 ]
