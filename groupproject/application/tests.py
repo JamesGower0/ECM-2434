58297b06c9330581dcf0dc3c33cc4546774f4672
mafarl <marie.v.frolova@gmail.com>
2024-02-26 16:00:49 +0000

    tests-finished

--- groupproject/application/tests.py
+++ groupproject/application/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .forms import SignUpForm
+from .forms import RegisterForm
+from django.contrib.auth.models import User
 
 
 # Create your tests here.
@@ -11,54 +12,52 @@ class RegisterFormTest(TestCase):
         """
         Checks if a user can sign up using a valid password
         """
-        form = SignUpForm(data={"username":'testUser', "email":"testEmail@email.com",
-                                "password1":"exeternest###1234a", "password2":"exeternest###1234a",
-                                "avatar_choice": ('robin', 'Avatar 1')})
+        form = RegisterForm(data={"username":'testUser', "email":"testEmail@email.com",
+                                "password1":"exeternest!454#a", "password2":"exeternest!454#a",
+                                "avatar_choice": 'robin'})
         self.assertTrue(form.is_valid())
 
     def testInvalidPassword(self):
         """
         Checks that the form rejects invalid passwords
         """
-        form = SignUpForm(data={"username":'testUser', "email":"testEmail@email.com",
+        form = RegisterForm(data={"username":'testUser', "email":"testEmail@email.com",
                                 "password1":"password", "password2":"password",
-                                "avatar_choice": ('robin', 'Avatar 1')})
+                                "avatar_choice": 'robin'})
         self.assertFalse(form.is_valid())
 
     def testDuplicateUsers(self):
         """
         Checks that two users cant be created with the same username
         """
-        form = SignUpForm(data={"username":'testUser', "email":"testEmail@email.com",
-                                "password1":"exeternest###1234a", "password2":"exeternest###1234a",
-                                "avatar_choice": ('robin', 'Avatar 1')})
-        form.save()
-        form2 = SignUpForm(data={"username":'testUser', "email":"testEmail@email.com",
+        user = User.objects.create_user(username='testUser', email='testEmail@email.com', password='exeternest###1234a')
+        
+        form2 = RegisterForm(data={"username":'testUser', "email":"testEmail@email.com",
                                  "password1":"exeternest###1234a", "password2":"exeternest###1234a",
-                                 "avatar_choice": ('robin', 'Avatar 1')})
+                                 "avatar_choice": 'robin'})
         self.assertFalse(form2.is_valid())
 
     def testEmptyUsernameField(self):
         """
         Checks that a username is required
         """
-        form = SignUpForm(data={"username":'', "email":"testEmail@email.com",
+        form = RegisterForm(data={"username":'', "email":"testEmail@email.com",
                                 "password1":"exeternest###1234a", "password2":"exeternest###1234a",
-                                "avatar_choice": ('robin', 'Avatar 1')})
+                                "avatar_choice": 'robin'})
         self.assertFalse(form.is_valid())
 
     def testEmptyEmailField(self):
         """
         Checks that a email is required
         """
-        form = SignUpForm(data={"username":'testUser', "email":"", "password1":"exeternest###1234a",
-                                "password2":"exeternest###1234a", "avatar_choice": ('robin', 'Avatar 1')})
+        form = RegisterForm(data={"username":'testUser', "email":"", "password1":"exeternest###1234a",
+                                "password2":"exeternest###1234a", "avatar_choice": 'robin'})
         self.assertFalse(form.is_valid())
 
     def testEmptyPasswordField(self):
         """
         Checks that a password is required
         """
-        form = SignUpForm(data={"username":'testUser', "email":"testEmail@email.com",
-                                "password1":"", "password2":"", "avatar_choice": ('robin', 'Avatar 1')})
+        form = RegisterForm(data={"username":'testUser', "email":"testEmail@email.com",
+                                "password1":"", "password2":"", "avatar_choice": 'robin'})
         self.assertFalse(form.is_valid())
