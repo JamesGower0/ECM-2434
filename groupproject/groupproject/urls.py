799c0340535993e32efab3feb265d506ce260553
mafarl <marie.v.frolova@gmail.com>
2024-02-26 16:24:21 +0000

    changes-from-main

--- groupproject/groupproject/urls.py
+++ groupproject/groupproject/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from application import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -26,4 +27,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("map/", views.map, name="map"),
     path("", views.navBar, name="navBar"),
+    path("profile/", views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name = 'login'),
+    path('logout/', views.logout_view, name = 'logout'),
 ]
