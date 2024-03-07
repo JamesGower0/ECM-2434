2cb668374aa20314597820b935171f10f117bbb9
te335 <114595444+te335@users.noreply.github.com>
2024-03-07 13:39:01 +0000

    Update urls.py
    removed superfluous urls

--- groupproject/application/urls.py
+++ groupproject/application/urls.py
@@ -7,13 +7,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("qr/", views.qr, name="qr"),
+    path("qr/questions", views.qr, name="qr"),
     path("qr/scan/",views.scan,name = "scan"),
-    path("qr/questions1/", views.questions1, name="questions1"),
-    path("qr/questions2/", views.questions2, name="questions2"),
-    path("qr/questions3/", views.questions3, name="questions3"),
-    path("qr/questions4/", views.questions4, name="questions4"),
-    path("qr/questions5/", views.questions5, name="questions5"),
     path("qr/correct/",views.correct_answer,name = "correct"),
     path("qr/wrong/",views.wrong_answer,name="wrong"),
     path("home/", views.home, name="home"),
