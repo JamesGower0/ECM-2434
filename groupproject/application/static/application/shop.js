f6257c4d1b8d70fdb2ef0c713a43fea1de964d0d
mafarl <marie.v.frolova@gmail.com>
2024-03-18 18:40:12 +0000

    fixed

--- groupproject/application/static/application/shop.js
+++ groupproject/application/static/application/shop.js
@@ -1,12 +1,21 @@
-const close = document.getElementById("close");
-const open = document.getElementById("open");
-const modal = document.getElementById("modal");
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Hellow");
+  const closeWindow = document.getElementById("close");
+  const open = document.getElementById("open");
+  const modal = document.getElementById("modal");
+  const container = document.querySelector(".modal-container");
+  const images = document.getElementsByClassName("item-shop");
 
-const images = document.getElementsByClassName("item-shop");
 
-images.addEventListener("click", () => images.classList.add("show-modal"));
-close.addEventListener("click", () => images.classList.remove("show-modal"));
 
-window.addEventListener("click", (e) => {
-  e.target === images ? images.classList.remove("show-modal") : false;
+  Array.from(images).forEach(image => {
+    image.addEventListener("click", function() {   
+      container.classList.add("show-modal");
+    });
+  });
+
+  closeWindow.addEventListener("click", function() {
+      container.classList.remove("show-modal");
+  });
+
 });
