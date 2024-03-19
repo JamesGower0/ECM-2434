fb978ffeca987c4c01eb2c6e1589f185c09302ae
mafarl <marie.v.frolova@gmail.com>
2024-03-19 12:48:10 +0000

    pop-up-displayed

--- groupproject/application/static/application/shop.js
+++ groupproject/application/static/application/shop.js
@@ -2,17 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("Hellow");
   const closeWindow = document.getElementById("close");
   const open = document.getElementById("open");
-  const modal = document.getElementById("modal");
   const container = document.querySelector(".modal-container");
   const images = document.getElementsByClassName("shop-img");
 
 
-
   Array.from(images).forEach(image => {
-    image.addEventListener("click", function() {   
+    image.addEventListener("click", function() {  
       const imgUrl = image.src;
-      open.dataset.imageUrl = image.alt;
       const alttext = image.alt;
+      
+      // Image displayed in pop up
+      const imgDisplayedInForm = document.getElementById("modal-item-img");
+      imgDisplayedInForm.src = imgUrl;
+      // Price of the image in pop up
+      const priceDisplayedInForm = document.getElementById("modal-item-price");
+      priceDisplayedInForm.textContent = alttext + " points";
       
       container.classList.add("show-modal");
     });
