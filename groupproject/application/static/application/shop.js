926929016d9a5347678009b10c37c7bd9dc7b2c3
Ashley <ashleyxcard@gmail.com>
2024-03-19 12:27:15 +0000

    a

--- groupproject/application/static/application/shop.js
+++ groupproject/application/static/application/shop.js
@@ -4,12 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const open = document.getElementById("open");
   const modal = document.getElementById("modal");
   const container = document.querySelector(".modal-container");
-  const images = document.getElementsByClassName("item-shop");
+  const images = document.getElementsByClassName("shop-img");
 
 
 
   Array.from(images).forEach(image => {
     image.addEventListener("click", function() {   
+      const imgUrl = image.src;
+      open.dataset.imageUrl = image.alt;
+      const alttext = image.alt;
+      
       container.classList.add("show-modal");
     });
   });
